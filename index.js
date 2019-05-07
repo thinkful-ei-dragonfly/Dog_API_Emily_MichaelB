c5c83d5499ac468be2e584452e20c96d63c94817
Michael Bravo <michael.bravo777@gmail.com>
2019-05-07 16:10:09 -0500

    finished checkpoint 5

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const state = {
     img: [],
+    breeds: [],
     num: 0,
 };
 
@@ -10,46 +11,50 @@ const state = {
 function createOptionList() {
   let value = ""
   for (let i = 1; i <= 50; i++) {
-
     value += `<option value=${i}>${i}</option>`;
   }
   $('#num-selector').html(value);
-    
+};
+
+function fetchBreedList() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+  .then(response => response.json())
+  .then(responseJson => {
+    let breed = '';
+    for (breed in responseJson.message) {
+      state.breeds.push(breed);
+      createBreedList();
+    }
+  })
+
+  
+}
+
+function createBreedList() {
+    let value = ""
+    for (let i = 0; i < state.breeds.length; i++) {
+      value += `<option value=${state.breeds[i]}>${state.breeds[i]}</option>`;
+    }
+    $('#breed-selector').html(value);
   };
-
-
-
-
-
-
-
+  
 
 function render() {
-    const imgTag = state.img ? `<img class="image" src="${state.img}" alt="Random Image">` : '<img />';
-
-    const html = `
-    ${imgTag}
-    <span class="caption">${state.caption}</span>
-  `;
-
-    $('.image-container').html(html);
+    let imgTag = "";
+    for (let i = 0; i < state.img.length; i++) {
+      imgTag += `<img class="results-img" src="${state.img[i]}" alt="Random Image">`;
+    }
+    $('#dogImages').html(imgTag);
 }
 
 function handleRandomClick() {
     $('form').submit(event => {
       event.preventDefault();
       state.num = $('#num-selector option:selected').text();
-        console.log(getRandomDogs());   
-            // .then(response => {
-            //     state.img = response.data.image_url;
-            //     render();
-          
+        getRandomDogs();
+        render();
     });
 }
-
-
-
-
 
 // function handleCaptionChange() {
 //     $('#caption-input').on('keyup', e => {
@@ -59,17 +64,21 @@ function handleRandomClick() {
 // }
 
 
-
 function getRandomDogs() {
-  return fetch("https://dog.ceo/api/breeds/image/random")
+  // return fetch("https://dog.ceo/api/breeds/image/random/" + state.num)
+  return fetch("https://dog.ceo/api/breed/" +  $('#breed-selector option:selected').text() + "/images/random/" + state.num)
         .then(response => response.json())
 
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => {
+          state.img = responseJson.message;
+          render();
+        })
         };
 
 
 $(() => {
     createOptionList();
+    fetchBreedList();
     handleRandomClick();
     
     render();
